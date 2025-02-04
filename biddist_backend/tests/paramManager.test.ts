5a1d1dc1d3634acc614b72b7f8adf6f183016cd6
Golos1 <20acann@gmail.com>
2025-02-04 12:01:26 -0500

    updating tests to give descriptions

--- biddist_backend/tests/paramManager.test.ts
+++ biddist_backend/tests/paramManager.test.ts
@@ -1,14 +1,17 @@
 import paramStore from "../src/paramManager"
-test('check to see param initialization',async ()=>{
-    const params = await paramStore.getAllParams();
-    expect(params.mailgun_domain).toBeDefined();
-    expect(params.mailgun_domain.length).toBeGreaterThan(0);
-    expect(params.mailgun).toBeDefined();
-    expect(params.mailgun.length).toBeGreaterThan(0);
-    expect(params.db_conn).toBeDefined();
-    expect(params.db_conn.length).toBeGreaterThan(0)
-    expect(params.stripe).toBeDefined();
-    expect(params.stripe.length).toBeGreaterThan(0);
-    expect(params.frontend_domain).toBeDefined();
-    expect(params.frontend_domain.length).toBeGreaterThan(0);
-})
+
+describe('testing to see if program parameters like API keys are properly fetched', () => {
+    test('check to see param initialization',async ()=>{
+        const params = await paramStore.getAllParams();
+        expect(params.mailgun_domain).toBeDefined();
+        expect(params.mailgun_domain.length).toBeGreaterThan(0);
+        expect(params.mailgun).toBeDefined();
+        expect(params.mailgun.length).toBeGreaterThan(0);
+        expect(params.db_conn).toBeDefined();
+        expect(params.db_conn.length).toBeGreaterThan(0)
+        expect(params.stripe).toBeDefined();
+        expect(params.stripe.length).toBeGreaterThan(0);
+        expect(params.frontend_domain).toBeDefined();
+        expect(params.frontend_domain.length).toBeGreaterThan(0);
+    })
+});
--- biddist_backend/tests/services.test.ts
+++ biddist_backend/tests/services.test.ts
@@ -1,9 +1,12 @@
 import {Services} from "../src/services";
-test("check to see if singletons are initialized properly. Requires env file!", async ()=>{
-    const mailgun = await Services.getMailgun();
-    const mailgun2 = await Services.getMailgun();
-    expect(mailgun).toBe(mailgun2);
-    const stripe = await Services.getStripe();
-    const stripe2 = await Services.getStripe();
-    expect(stripe).toBe(stripe2);
-});
+describe("testing external services",()=>{
+    test("check to see if singletons are initialized properly. Requires env file!", async ()=>{
+        const mailgun = await Services.getMailgun();
+        const mailgun2 = await Services.getMailgun();
+        expect(mailgun).toBe(mailgun2);
+        const stripe = await Services.getStripe();
+        const stripe2 = await Services.getStripe();
+        expect(stripe).toBe(stripe2);
+    });
+})
+
