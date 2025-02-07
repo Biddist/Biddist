2788e43f92351a8342870b8238867c102324148c
Golos1 <20acann@gmail.com>
2025-02-07 10:47:03 -0500

    added ecr infra

--- main.tf
+++ main.tf
@@ -26,3 +26,13 @@ resource "aws_s3_bucket" "backend_bucket" {
     prevent_destroy = true
   }
 }
+resource "aws_ecr_repository" "biddist_registry" {
+  name = "biddist_registry"
+  force_delete = false
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+output "ecr_url" {
+  value = aws_ecr_repository.biddist_registry.repository_url
+}
