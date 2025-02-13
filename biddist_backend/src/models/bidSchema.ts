329d2a2bf0177060c23dd98cd2301a03e6396807
Golos1 <20acann@gmail.com>
2025-02-13 15:06:39 -0500

    updated Bid schema to reflect new payments flow - storing paymentMethods wouldn't be a good practice, so instead, Accounts with unpaid Bids will have to pay them before they can take any other actions; session data will be used to track which accounts have unpaid bids.

--- biddist_backend/src/models/bidSchema.ts
+++ biddist_backend/src/models/bidSchema.ts
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from 'mongoose'
 import {IBid} from '../interfaces/Imodels';
+import {Services} from "../services";
 
 const bidSchema = new Schema<IBid>({
     Bidder: {
@@ -16,11 +17,38 @@ const bidSchema = new Schema<IBid>({
         type: Number,
         required: true,
     },
-    PaymentMethodId: {
-        type: String,
-        required: true,
+    Paid: {
+        type: Boolean,
+        default: false
+    },
+    IsWinningBid: {
+        type: Boolean,
+        default: false
     }
 });
+
+/**
+ * Creates and confirms a Stripe PaymentIntent to pay for a successful bid.
+ * @param paymentMethodId the Id of the customer's payment method, used to make the payment intent.
+   @return true if the payment succeeded, false otherwise.
+ */
+bidSchema.methods.payBid = async function(paymentMethodId: string): Promise<boolean> {
+    const stripe = await Services.getStripe();
+    const result = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: this.Money,
+        confirm: true,
+        payment_method: paymentMethodId
+    });
+    if(result.status == 'succeeded'){
+        this.Paid = true;
+        await this.save();
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 const Bid = mongoose.model("Bid",bidSchema);
 export default Bid;
 export type {IBid};
