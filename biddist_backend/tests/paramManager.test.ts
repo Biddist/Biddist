7f535c593c1c0f60d1887ab2e50237049370fea5
Golos1 <20acann@gmail.com>
2025-03-27 19:45:48 -0400

    accounts can now be created; migrated from mailgun to mailtrap.

--- biddist_backend/tests/paramManager.test.ts
+++ biddist_backend/tests/paramManager.test.ts
@@ -3,10 +3,8 @@ import paramStore from "../src/paramManager"
 describe('testing to see if program parameters like API keys are properly fetched', () => {
     test('check to see param initialization',async ()=>{
         const params = await paramStore.getAllParams();
-        expect(params.mailgun_domain).toBeDefined();
-        expect(params.mailgun_domain.length).toBeGreaterThan(0);
-        expect(params.mailgun).toBeDefined();
-        expect(params.mailgun.length).toBeGreaterThan(0);
+        expect(params.mailtrap).toBeDefined();
+        expect(params.mailtrap.length).toBeGreaterThan(0);
         expect(params.db_conn).toBeDefined();
         expect(params.db_conn.length).toBeGreaterThan(0)
         expect(params.stripe).toBeDefined();
